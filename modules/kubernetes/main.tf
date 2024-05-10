62ef66210db2ac4b17f9dced9885afb6001fe576
Hassan-Eid-Hassan <hassaneid339@gmail.com>
2024-05-10 05:21:05 +0300

    adding kubernets-native

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,31 +1,36 @@
-resource "aws_instance" "jenkins" {
-    ami           = var.jenkins_ami_id
-    instance_type = var.jenkins_instance_type
-    subnet_id     = var.jenkins_subnet_id
+resource "aws_instance" "k8s-master" {
+    ami           = var.k8s_master_ami_id
+    instance_type = var.k8s_master_instance_type
+    subnet_id     = var.k8s_master_subnet_id
     key_name      = var.key_name
-    vpc_security_group_ids = [var.jenkins_security_group_id]
+    vpc_security_group_ids = [var.k8s_master_security_group_id]
 
     tags = {
-        Name = "Jenkins"
+        Name = "k8s-master"
     }
 
     connection {
         type        = "ssh"
-        user        = var.jenkins_instance_user
+        user        = var.k8s_master_instance_user
         private_key = file(var.ssh_key_path)
-        host        = aws_instance.jenkins.public_ip
+        host        = aws_instance.k8s_master.public_ip
     }
 
-    # Provision Jenkins installation script
+    # Provision k8s_master installation script
     provisioner "file" {
-        source      = "${path.module}/jenkins_install.sh"
-        destination = "/tmp/jenkins_install.sh"
+        source      = "${path.module}/k8s_master_install.sh"
+        destination = "/tmp/k8s_master_install.sh"
     }
 
     provisioner "remote-exec" {
         inline = [
-            "sudo sed -i 's/\r$//' /tmp/jenkins_install.sh",
-            "sudo bash /tmp/jenkins_install.sh"
+            "sudo sed -i 's/\r$//' /tmp/k8s_master_install.sh",
+            "sudo bash /tmp/k8s_master_install.sh"
         ]
     }
+}
+
+# Read the join command from the master node
+data "external" "join_command" {
+  program = ["sh", "-c", "ssh -i ${var.ssh_key_path} ${var.k8s_master_instance_user}@${aws_instance.master.public_ip} 'cat /tmp/join_command.txt'"]
 }
